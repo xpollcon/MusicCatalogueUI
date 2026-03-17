79332f918ad2f042b2fd1dc78dda5b91b426fadd
Borzou <na>
2026-03-17 18:27:05 +1100

    Fix auth persistence on refresh

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{ redirect_uri: window.location.origin }}
+      cacheLocation="localstorage"
     >
       <ApolloWrapper>
         <App />
